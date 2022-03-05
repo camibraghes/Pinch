e88e71678f836517cc48d055a75febf639a19879
CamiBraghes <cameliabraghes@gmail.com>
2022-03-05 14:09:46 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Camelia Braghes on 05.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
